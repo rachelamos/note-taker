001035b4a9ff7542750039dfc82f5a7a526d1add
Rachel Amos <rachelamos35@gmail.com>
2021-03-19 13:00:21 -0500

    improving api route js file logic

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -5,12 +5,31 @@ const router = require('express').Router();
 router.get("/notes", (req, res) => res.json(dbData));
 // fs writeFile to read contents of my dbData (add above)
 router.post("/notes", (req, res) => {
-    dbData.push(req.body);
-    res.json(true);
-    const id = generateUniqueId({
-        length: 4,
-        useLetters: false
+    const newNote = {
+        "title": req.body.title,
+        "text": req.body.text,
+        "id": generateUniqueId(),
+    }
+    dbData.push(newNote);
+    console.log(`Saved notes in DB array ${dbData}`)
+
+    const newDbData = JSON.stringify(dbData);
+    console.log(`New DB of Notes: ${newDbData}`)
+
+    fs.writeFile(`${__dirname}/db/db.json`, `${newDbData}`, (err) => {
+        if (err) throw err;
+
+        res.writeHead(200, { 'Content-Type': 'application/json' });
+        res.end(`${newDbData}`);
+
+
     })
+    // dbData.push(req.body);
+    // res.json(true);
+    // const id = generateUniqueId({
+    //     length: 4,
+    //     useLetters: false
+    // })
 });
     // Bonus section potentially
     // app.post("/api/clear", (req, res) => {
