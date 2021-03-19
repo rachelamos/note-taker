74a3d3c5b04ff1899e46f5c531f42846381f0d33
Rachel Amos <rachelamos35@gmail.com>
2021-03-19 15:00:46 -0500

    attempts to correct logic

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,37 +1,62 @@
-const dbData = require('../db/db.json');
+const dbData = require('/../db/db.json');
 const generateUniqueId = require('generate-unique-id');
 const router = require('express').Router();
+const fs = require('fs');
 
-router.get("/notes", (req, res) => res.json(dbData));
+router.get("/notes", (req, res) => {
+    console.log(newNote);
+    fs.readFile(`${__dirname}/../db/db.json`, (err, data) => {
+        if (err) throw err;
+        res.writeHead(200, { 'Content-Type': 'application/json' });
+        res.end(data);
+    })
+});
+
 // fs writeFile to read contents of my dbData (add above)
 router.post("/notes", (req, res) => {
-    const newNote = {
-        "title": req.body.title,
-        "text": req.body.text,
-        "id": generateUniqueId(),
-    }
-    dbData.push(newNote);
-    console.log(`Saved notes in DB array ${dbData}`)
+    const newNote = JSON.stringify(req.body);
+        // "title": req.body.title,
+        // "text": req.body.text,
+    newNote.id = generateUniqueId(),
+    console.log(newNote);
 
-    const newDbData = JSON.stringify(dbData);
-    console.log(`New DB of Notes: ${newDbData}`)
-
-    fs.writeFile(`${__dirname}/db/db.json`, `${newDbData}`, (err) => {
+    fs.readFile(`${__dirname}/../db/db.json`, (err, data) => {
         if (err) throw err;
 
-        res.writeHead(200, { 'Content-Type': 'application/json' });
-        res.end(`${newDbData}`);
-
-
+        let savedNotes = JSON.parse(data);
+        console.log(savedNotes);
+        savedNotes.push(newNote);
+        fs.writeFile(`${__dirname}/../db/db.json`, JSON.stringify(savedNotes), (err, data) => {
+            if (err) throw err;
+            console.log("Saved note");
+            res.json(newNote);
+        })
     })
+})
+
+    // dbData.push(newNote);
+    // // push.JSON.stringify(newNote);
+    
+    // console.log(`Saved notes in DB array ${dbData}`)
+
+    // const newDbData = JSON.stringify(dbData);
+    // newDbData.push();
+    // console.log(`New DB of Notes: ${newDbData}`)
+
+    // fs.writeFile(`${__dirname}/../db/db.json`, `${newDbData}`, (err) => {
+    //     if (err) throw err;
+
+    //     res.writeHead(200, { 'Content-Type': 'application/json' });
+    //     res.end(`${newDbData}`);
+
+
+    // })
     // dbData.push(req.body);
     // res.json(true);
     // const id = generateUniqueId({
     //     length: 4,
     //     useLetters: false
-    // })
-});
-    // Bonus section potentially
+    // })// Bonus section potentially
     // app.post("/api/clear", (req, res) => {
     //     tableData.length = 0;
     //     waitlistData.length = 0;
