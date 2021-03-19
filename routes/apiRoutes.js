331b93ff250e92eb71c20b008751a0025fd13d9d
Rachel Amos <rachelamos35@gmail.com>
2021-03-19 16:07:51 -0500

    deleting commented out code

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,10 +1,8 @@
-// const dbData = require('/../db/db.json');
 const generateUniqueId = require('generate-unique-id');
 const router = require('express').Router();
 const fs = require('fs');
 
 router.get("/notes", (req, res) => {
-    // console.log(newNote);
     fs.readFile(`${__dirname}/../db/db.json`, (err, data) => {
         if (err) throw err;
         res.writeHead(200, { 'Content-Type': 'text/html' });
@@ -15,8 +13,7 @@ router.get("/notes", (req, res) => {
 // fs writeFile to read contents of my dbData (add above)
 router.post("/notes", (req, res) => {
     const newNote = req.body;
-        // "title": req.body.title,
-        // "text": req.body.text,
+
     newNote.id = generateUniqueId(),
     console.log(newNote);
 
@@ -33,35 +30,5 @@ router.post("/notes", (req, res) => {
         })
     })
 })
-
-    // dbData.push(newNote);
-    // // push.JSON.stringify(newNote);
-    
-    // console.log(`Saved notes in DB array ${dbData}`)
-
-    // const newDbData = JSON.stringify(dbData);
-    // newDbData.push();
-    // console.log(`New DB of Notes: ${newDbData}`)
-
-    // fs.writeFile(`${__dirname}/../db/db.json`, `${newDbData}`, (err) => {
-    //     if (err) throw err;
-
-    //     res.writeHead(200, { 'Content-Type': 'application/json' });
-    //     res.end(`${newDbData}`);
-
-
-    // })
-    // dbData.push(req.body);
-    // res.json(true);
-    // const id = generateUniqueId({
-    //     length: 4,
-    //     useLetters: false
-    // })// Bonus section potentially
-    // app.post("/api/clear", (req, res) => {
-    //     tableData.length = 0;
-    //     waitlistData.length = 0;
-    //     res.json({ok: true});
-    // });
-
 
 module.exports = router;
